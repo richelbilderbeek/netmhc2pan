647de05ddda4946578be1dcc7ac3d8d5b963cbe7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-28 09:04:30 +0100

    Thanks @lintr-bot, thanks @MangoTheCat

--- R/install_netmhc2pan.R
+++ R/install_netmhc2pan.R
@@ -1,6 +1,10 @@
 #' Install NetMHC2pan to a local folder
 #' @author Richel J.C. Bilderbeek
 #' @export
-install_netmhc2pan <- function() {
+install_netmhc2pan <- function(
+  folder_name = rappdirs::user_data_dir(),
+  verbose = FALSE,
+  os = rappdirs::app_dir()$os
+) {
 
 }
