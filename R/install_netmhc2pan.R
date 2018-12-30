b9504692115da9e39d7cd105d8c171f7fa6f4252
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-30 19:33:20 +0100

    Use temp download URL

--- R/install_netmhc2pan.R
+++ R/install_netmhc2pan.R
@@ -101,7 +101,7 @@ install_netmhc2pan_data <- function(
 
   dir.create(path = folder_name, showWarnings = FALSE, recursive = TRUE)
 
-  url <- "http://www.cbs.dtu.dk/services/NetMHCIIpan-3.2/data.Linux.tar.gz"
+  url <- get_netmhc2pan_url()
   if (verbose == TRUE) {
     print(paste("Download from URL:", url))
   }
