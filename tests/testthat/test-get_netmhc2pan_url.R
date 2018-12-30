608435cec1d57c368e235fa82846d70530e116cf
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-30 20:16:00 +0100

    A URL is a character vector

--- tests/testthat/test-get_netmhc2pan_url.R
+++ tests/testthat/test-get_netmhc2pan_url.R
@@ -1,10 +1,5 @@
 context("test-get_netmhc2pan_url")
 
 test_that("use", {
-    expect_true(
-    grepl(
-      "(http://richelbilderbeek.nl/tmp_tmhmm-2.0c.Linux.tar.gz)|http://www.cbs.dtu.dk/download/[0-9A-F-]*$)",
-      get_netmhc2pan_url()
-    )
-  )
+  expect_equal("character", get_netmhc2pan_url())
 })
