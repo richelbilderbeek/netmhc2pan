54abb3dc0eecbedbec29547aa23c50ee1313642e
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-31 16:05:12 +0100

    Error handling better ordered

--- R/get_netmhc2pan_alleles.R
+++ R/get_netmhc2pan_alleles.R
@@ -24,8 +24,9 @@ get_netmhc2pan_alleles <- function(
       stop(
         "Call to NetMHC2pan failed\n",
         "bin_file_path: ", bin_file_path, "\n",
-        "text:\n" , text, "\n",
-        "error:\n", msg
+        "error:\n", msg, "\n",
+        "\n",
+        "text:\n", text, "\n"
       )
     }
   )
