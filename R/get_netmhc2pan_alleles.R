308a9562302bc1e3ad2f6b2e1d44501be2254337
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-31 16:02:38 +0100

    More verbose error handling

--- R/get_netmhc2pan_alleles.R
+++ R/get_netmhc2pan_alleles.R
@@ -13,14 +13,14 @@ get_netmhc2pan_alleles <- function(
   testit::assert(is_netmhc2pan_installed()) # nolint netmhc2pan function
   bin_file_path <- file.path(folder_name, "netMHCIIpan-3.2", "netMHCIIpan")
   testit::assert(file.exists(bin_file_path))
-  tryCatch(
-    text <- system2(
-      command = bin_file_path,
-      args = c("-list"),
-      stdout = TRUE
-    ),
-    error = function(msg) 
-    {
+  tryCatch({
+      text <- system2(
+        command = bin_file_path,
+        args = c("-list"),
+        stdout = TRUE
+      )
+    },
+    error = function(msg) {
       stop(
         "Call to NetMHC2pan failed\n",
         "bin_file_path: ", bin_file_path, "\n",
