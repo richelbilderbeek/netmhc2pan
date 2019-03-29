ae9e6de6aa449ebdaa73fbbbf9041461afdb7e2b
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-29 11:33:09 +0100

    Tests use NetMHC2pan

--- tests/testthat/test-get_default_netmhc2pan_subfolder.R
+++ tests/testthat/test-get_default_netmhc2pan_subfolder.R
@@ -1,10 +1,8 @@
 context("test-get_default_netmhc2pan_subfolder")
 
 test_that("use", {
-  expect_true(
-    grepl(
-      "netMHCpan-4.0",
-      get_default_netmhc2pan_subfolder()
-    )
+  expect_equal(
+    basename(get_default_netmhc2pan_subfolder()),
+    "netMHCIIpan-3.2"
   )
 })
