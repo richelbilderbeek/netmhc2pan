87ceab0512f19601db2cab7721d9371628cc916a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-03-29 08:25:02 +0100

    Fresh URL

--- R/get_netmhc2pan_url.R
+++ R/get_netmhc2pan_url.R
@@ -4,5 +4,5 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_netmhc2pan_url <- function() {
-  "http://www.cbs.dtu.dk/download/33A6B0AC-0F2E-11E9-B4D1-8ABCB9CD16B5/"
+  "http://www.cbs.dtu.dk/download/3A5D00C4-51F3-11E9-96CC-56EEB9CD16B5/"
 }
