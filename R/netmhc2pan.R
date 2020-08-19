30af75fd9cbb08a20454434a1335d1fb4d857b3c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-19 12:29:58 +0200

    Fix doc

--- R/netmhc2pan.R
+++ R/netmhc2pan.R
@@ -1,4 +1,11 @@
-#' netmhc2pan: predict peptide binfing strengths using NetMHC2pan
+#' netmhc2pan: predict peptide binding strengths using NetMHC2pan
+#'
+#' 'NetMHCIIpan' is a tool to predict the
+#' binding strength of a short peptide to an MHC-II complex.
+#' 'NetMHCIIpan' can be used from a web server at
+#' 'https://services.healthtech.dtu.dk/service.php?NetMHCIIpan-3.2'
+#' or from the command-line, using a local installation. This package
+#' allows to call 'NetMHCIIpan' from R.
 #' @examples
 #' if (is_netmhc2pan_installed()) {
 #'
