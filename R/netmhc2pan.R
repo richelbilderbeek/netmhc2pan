94fcfcf6210510e4a273e2ccc801620d0651d899
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-19 12:12:32 +0200

    Add pkg doc

---
+++ R/netmhc2pan.R
@@ -0,0 +1,13 @@
+#' netmhc2pan: predict peptide binfing strengths using NetMHC2pan
+#' @examples
+#' if (is_netmhc2pan_installed()) {
+#'
+#'   predict_ic50(
+#'     peptides = c("AIAACAMLLV", "ALVCYIVMPV"),
+#'     mhc_haplotype = "DRB1_0416"
+#'   )
+#' }
+#' @author Richèl J.C. Bilderbeek
+#' @docType package
+#' @name netmhc2pan
+NULL
