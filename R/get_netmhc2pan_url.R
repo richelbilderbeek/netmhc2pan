9a2eeb7cd81ddda81e98efcc277ba47cd30c6198
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-20 08:51:32 +0200

    Fresh URL

--- R/get_netmhc2pan_url.R
+++ R/get_netmhc2pan_url.R
@@ -7,5 +7,5 @@
 #' @author Richèl J.C. Bilderbeek
 #' @export
 get_netmhc2pan_url <- function() {
-  "https://services.healthtech.dtu.dk/download/eb3616d8-23fc-4752-af89-971070e7a255" # nolint indeed a long URL
+  "https://services.healthtech.dtu.dk/download/9512eb7c-5f63-48d8-b3f5-22434f8aa7c4" # nolint indeed a long URL
 }
