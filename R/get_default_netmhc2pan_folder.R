f55257b0007efc0ce1f52c79ebb3697d0799c70c
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-24 10:20:53 +0200

    Process rhub feedback

--- R/get_default_netmhc2pan_folder.R
+++ R/get_default_netmhc2pan_folder.R
@@ -3,14 +3,7 @@
 #' @return the path to the folder where this package installs
 #'   NetMHCIIpan by default
 #' @examples
-#'   if (rappdirs::app_dir()$os == "unix") {
-#'     testit::assert(
-#'       grepl(
-#'         "/home/[A-Za-z0-9_]*/.local/share",
-#'         get_default_netmhc2pan_folder()
-#'       )
-#'     )
-#'   }
+#' get_default_netmhc2pan_folder()
 #' @author Richèl J.C. Bilderbeek
 #' @export
 get_default_netmhc2pan_folder <- function() {
