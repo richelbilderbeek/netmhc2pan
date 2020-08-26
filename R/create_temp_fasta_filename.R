87097f8919bef1cb222b596db82242980d6ab005
richelbilderbeek <richel@richelbilderbeek.nl>
2020-08-26 15:20:54 +0200

    Fix

--- R/create_temp_fasta_filename.R
+++ R/create_temp_fasta_filename.R
@@ -1,6 +1,6 @@
 #' Create a \code{.fasta} file
 #' @export
-create_temp_xls_filename <- function() {
+create_temp_fasta_filename <- function() {
   tempfile(
     pattern = "temp_",
     tmpdir = rappdirs::user_cache_dir(),
