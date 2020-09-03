993a99e484d89c9c801122c1a2f547836230b160
richelbilderbeek <richel@richelbilderbeek.nl>
2020-09-03 10:26:50 +0200

    Improve doc

--- R/capitalize_first_char.R
+++ R/capitalize_first_char.R
@@ -1,6 +1,12 @@
-#' Convert the first character to upper case
+#' Convert the first character to upper case.
+#'
+#' Convert the first character to upper case. If the first
+#' character is upper case, nothing will happen.
 #' @param s a string
+#' @return the string, with the first letter in uppercase
 #' @author Richèl J.C. Bilderbeek
+#' @examples
+#' capitalize_first_char("hello")
 #' @export
 capitalize_first_char <- function(s) {
   testit::assert(is.character(s))
--- R/create_temp_xls_filename.R
+++ R/create_temp_xls_filename.R
@@ -1,4 +1,9 @@
 #' Create a \code{.xls} file
+#' @return a path to a non-existing file,
+#' for example, \code{/home/myusername/.cache/temp_582047dac733.xls}
+#' @examples
+#' create_temp_xls_filename()
+#' @author Richèl J.C. Bilderbeek
 #' @export
 create_temp_xls_filename <- function() {
   tempfile(
