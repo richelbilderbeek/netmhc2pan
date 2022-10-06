91ac6d2de753428ce306a6e2cdcbf5e55e42f24e
richelbilderbeek <richel@richelbilderbeek.nl>
2022-10-06 13:08:41 +0200

    Fix tests

--- tests/testthat/test-check_netmhc2pan_data_url.R
+++ tests/testthat/test-check_netmhc2pan_data_url.R
@@ -1,17 +1,21 @@
 test_that("use", {
   if (!is_on_ci()) return()
 
-  # This test will break if www.cbs.dtu.dk is down,
-  # here I assume it is usually up :-)
-  expect_silent(check_netmhc2pan_data_url())
+  if (is_url_valid()) {
+    # This test will break if www.cbs.dtu.dk is down,
+    # here I assume it is usually up :-)
+    expect_silent(check_netmhc2pan_data_url())
+  }
 })
 
 test_that("be verbose", {
   if (!is_on_ci()) return()
-  expect_message(
-    check_netmhc2pan_data_url(verbose = TRUE),
-    "netmhc2pan_data_url"
-  )
+  if (is_url_valid()) {
+    expect_message(
+      check_netmhc2pan_data_url(verbose = TRUE),
+      "netmhc2pan_data_url"
+    )
+  }
 })
 
 test_that("error message for invalid URL", {
