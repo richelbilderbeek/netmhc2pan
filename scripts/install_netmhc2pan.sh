b971107b46d9e68135961a3fdbe22c73c0aed2c7
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-08 17:45:28 +0100

    Fix bash style

--- scripts/install_netmhc2pan.sh
+++ scripts/install_netmhc2pan.sh
@@ -10,14 +10,16 @@
 #  ./scripts/install_netmhc2pan.sh
 #
 #
-cat R/install_netmhc2pan.R > tempscript.R
-cat R/capitalize_first_char.R >> tempscript.R
-cat R/get_default_netmhc2pan_bin_path.R >> tempscript.R
-cat R/get_default_netmhc2pan_folder.R >> tempscript.R
-cat R/get_default_netmhc2pan_subfolder.R >> tempscript.R
-cat R/get_netmhc2pan_bin_url.R >> tempscript.R
-cat R/is_netmhc2pan_installed.R >> tempscript.R
-echo "install_netmhc2pan()" >> tempscript.R
+{
+  cat R/install_netmhc2pan.R;
+  cat R/capitalize_first_char.R;
+  cat R/get_default_netmhc2pan_bin_path.R;
+  cat R/get_default_netmhc2pan_folder.R;
+  cat R/get_default_netmhc2pan_subfolder.R;
+  cat R/get_netmhc2pan_bin_url.R;
+  cat R/is_netmhc2pan_installed.R;
+  echo "install_netmhc2pan()";
+} > tempscript.R
 sed -i'.orginal' "s/netmhc2pan:://g" tempscript.R
 cat tempscript.R
 Rscript tempscript.R
