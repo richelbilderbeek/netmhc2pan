67c4c7a48737e3844a25e0b81459e3da3ceed5cf
richelbilderbeek <richel@richelbilderbeek.nl>
2023-11-08 15:07:16 +0100

    Deprecated

--- tests/testthat/test-set_up_netmhc2pan.R
+++ tests/testthat/test-set_up_netmhc2pan.R
@@ -1,23 +1,7 @@
-test_that("set_up_netmhc2pan", {
+test_that("deprecated", {
 
-  if (!is_on_ci()) return()
-  if (!is_url_valid()) return()
-
-  netmhc2pan_folder_name <- tempfile()
-  if (dir.exists(netmhc2pan_folder_name)) {
-    unlink(netmhc2pan_folder_name, recursive = TRUE)
-  }
-  expect_true(!dir.exists(netmhc2pan_folder_name))
-
-  # Empty folder
   expect_error(
-    set_up_netmhc2pan(netmhc2pan_folder_name = netmhc2pan_folder_name),
-    "NetMHCIIpan binary is absent"
-  )
-
-  # Minimal install
-  install_netmhc2pan_bin(netmhc2pan_folder_name = netmhc2pan_folder_name)
-  expect_silent(
-    set_up_netmhc2pan(netmhc2pan_folder_name = netmhc2pan_folder_name)
+    set_up_netmhc2pan(),
+    "Deprecated"
   )
 })
